ee98c109f2e59fc3cfc3bd19b5b8dec631ef47ea
Daniel Alm <Daniel.Alm@ForumD.net>
2015-11-07 12:21:15 +0100

    Initial Commit

---
+++ DateRangePicker/DateRangePicker.h
@@ -0,0 +1,19 @@
+//
+//  DateRangePicker.h
+//  DateRangePicker
+//
+//  Created by Daniel Alm on 07.11.15.
+//  Copyright © 2015 Daniel Alm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DateRangePicker.
+FOUNDATION_EXPORT double DateRangePickerVersionNumber;
+
+//! Project version string for DateRangePicker.
+FOUNDATION_EXPORT const unsigned char DateRangePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateRangePicker/PublicHeader.h>
+
+
