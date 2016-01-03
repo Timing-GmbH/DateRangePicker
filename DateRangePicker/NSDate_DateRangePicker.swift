a10d5ba8d5d270d9053865547796c52ca3586126
Daniel Alm <Daniel.Alm@ForumD.net>
2016-01-03 18:21:54 +0100

    Remove the NSDate extension methods that do not take an NSCalendar argument, but provide a default value for that argument instead.

--- DateRangePicker/NSDate_DateRangePicker.swift
+++ DateRangePicker/NSDate_DateRangePicker.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public extension NSDate {
-	public func drp_addCalendarUnits(count: Int, _ unit: NSCalendarUnit, calendar: NSCalendar) -> NSDate? {
+	public func drp_addCalendarUnits(count: Int, _ unit: NSCalendarUnit, calendar: NSCalendar = NSCalendar.currentCalendar()) -> NSDate? {
 		let advancedDate: NSDate?
 		if unit == .Quarter {
 			// There seems to be a bug where adding one quarter to a date in the 3rd quarter does not return the
@@ -20,34 +20,22 @@ public extension NSDate {
 		}
 		return advancedDate
 	}
-	public func drp_addCalendarUnits(count: Int, _ unit: NSCalendarUnit) -> NSDate? {
-		return drp_addCalendarUnits(count, unit, calendar: NSCalendar.currentCalendar())
-	}
 	
-	public func drp_beginningOfCalendarUnit(unit: NSCalendarUnit, calendar: NSCalendar) -> NSDate? {
+	public func drp_beginningOfCalendarUnit(unit: NSCalendarUnit, calendar: NSCalendar = NSCalendar.currentCalendar()) -> NSDate? {
 		var reducedDate: NSDate?
 		calendar.rangeOfUnit(unit, startDate: &reducedDate, interval: nil, forDate: self)
 		return reducedDate
 	}
-	public func drp_beginningOfCalendarUnit(unit: NSCalendarUnit) -> NSDate? {
-		return drp_beginningOfCalendarUnit(unit, calendar: NSCalendar.currentCalendar())
-	}
 	
-	public func drp_endOfCalendarUnit(unit: NSCalendarUnit, calendar: NSCalendar) -> NSDate? {
+	public func drp_endOfCalendarUnit(unit: NSCalendarUnit, calendar: NSCalendar = NSCalendar.currentCalendar()) -> NSDate? {
 		guard let startDate = drp_beginningOfCalendarUnit(unit, calendar: NSCalendar.currentCalendar()) else { return nil }
 		return startDate.drp_addCalendarUnits(1, unit, calendar: calendar)?.dateByAddingTimeInterval(-1)
 	}
-	public func drp_endOfCalendarUnit(unit: NSCalendarUnit) -> NSDate? {
-		return drp_endOfCalendarUnit(unit, calendar: NSCalendar.currentCalendar())
-	}
 	
 	// Returns the number of calendar days between the argument and the receiver.
-	public func drp_daysSince(since: NSDate, calendar: NSCalendar) -> Int {
+	public func drp_daysSince(since: NSDate, calendar: NSCalendar = NSCalendar.currentCalendar()) -> Int {
 		let fromDate = since.drp_beginningOfCalendarUnit(.Day, calendar: calendar)!
 		let toDate = self.drp_beginningOfCalendarUnit(.Day, calendar: calendar)!
 		return calendar.components(.Day, fromDate: fromDate, toDate: toDate, options: []).day
-	}
-	public func drp_daysSince(since: NSDate) -> Int {
-		return drp_daysSince(since, calendar: NSCalendar.currentCalendar())
 	}
 }
