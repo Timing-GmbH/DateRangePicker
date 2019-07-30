873db901c8cbbffcfd3509c91f1f2cad8c3db50e
Daniel Alm <Daniel.Alm@ForumD.net>
2019-07-30 16:59:54 +0200

    Fix some Swift 5 warnings.

--- DateRangePicker/NSDate_DateRangePicker.swift
+++ DateRangePicker/NSDate_DateRangePicker.swift
@@ -10,7 +10,7 @@ import Foundation
 
 //! CLEANUP: Migrate these APIs from NSCalendar.Unit to Calendar.Component.
 public extension Date {
-	public func drp_addCalendarUnits(_ count: Int, unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> Date? {
+	func drp_addCalendarUnits(_ count: Int, unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> Date? {
 		let advancedDate: Date?
 		if unit == .quarter {
 			// There seems to be a bug where adding one quarter to a date in the 3rd quarter does not return the
@@ -22,13 +22,13 @@ public extension Date {
 		return advancedDate
 	}
 	
-	public func drp_beginning(ofCalendarUnit unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> Date? {
+	func drp_beginning(ofCalendarUnit unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> Date? {
 		var reducedDate: NSDate?
 		(calendar as NSCalendar).range(of: unit, start: &reducedDate, interval: nil, for: self)
 		return reducedDate as Date?
 	}
 	
-	public func drp_end(ofCalendarUnit unit: NSCalendar.Unit, calendar: Calendar = Calendar.current,
+	func drp_end(ofCalendarUnit unit: NSCalendar.Unit, calendar: Calendar = Calendar.current,
 	                    adjustByOneSecond: Bool = true, returnNextIfAtBoundary: Bool = true) -> Date? {
 		guard let startDate = drp_beginning(ofCalendarUnit: unit, calendar: Calendar.current) else { return nil }
 		if startDate == self && !adjustByOneSecond && !returnNextIfAtBoundary { return self }
@@ -36,7 +36,7 @@ public extension Date {
 		return adjustByOneSecond ? result?.addingTimeInterval(-1) : result
 	}
 	
-	public func drp_calendarUnits(since: Date, unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> Int? {
+	func drp_calendarUnits(since: Date, unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> Int? {
 		guard let fromDate = since.drp_beginning(ofCalendarUnit: unit, calendar: calendar),
 			let toDate = self.drp_beginning(ofCalendarUnit: unit, calendar: calendar)
 			else { return nil }
@@ -46,13 +46,13 @@ public extension Date {
 	}
 	
 	// Returns the number of calendar days between the argument and the receiver.
-	public func drp_daysSince(_ since: Date, calendar: Calendar = Calendar.current) -> Int? {
+	func drp_daysSince(_ since: Date, calendar: Calendar = Calendar.current) -> Int? {
 		return self.drp_calendarUnits(since: since, unit: .day, calendar: calendar)
 	}
 }
 
 public extension Calendar.Component {
-	public var isAffectedByHourShift: Bool {
+	var isAffectedByHourShift: Bool {
 		switch self {
 		case .era, .year, .month, .day, .weekday, .weekdayOrdinal, .quarter, .weekOfMonth,
 		     .weekOfYear, .yearForWeekOfYear: return true
@@ -62,7 +62,7 @@ public extension Calendar.Component {
 }
 
 public extension Date {
-	public func drp_adding(_ value: Int, component: Calendar.Component, calendar: Calendar = Calendar.current) -> Date? {
+	func drp_adding(_ value: Int, component: Calendar.Component, calendar: Calendar = Calendar.current) -> Date? {
 		if component == .quarter {
 			// There seems to be a bug where adding one quarter to a date in the 3rd quarter does not return the
 			// corresponding date in the 4th quarter. As a workaround, we add 3 months instead.
@@ -72,25 +72,25 @@ public extension Date {
 		}
 	}
 	
-	public func drp_beginning(of component: Calendar.Component, calendar: Calendar = Calendar.current) -> Date? {
+	func drp_beginning(of component: Calendar.Component, calendar: Calendar = Calendar.current) -> Date? {
 		var startDate = self
 		var timeInterval: TimeInterval = 0
 		guard calendar.dateInterval(of: component, start: &startDate, interval: &timeInterval, for: self) else { return nil }
 		return startDate
 	}
 	
-	public func drp_end(of component: Calendar.Component, calendar: Calendar = Calendar.current,
+	func drp_end(of component: Calendar.Component, calendar: Calendar = Calendar.current,
 	                    returnNextIfAtBoundary: Bool = true) -> Date? {
 		guard let startDate = drp_beginning(of: component, calendar: Calendar.current) else { return nil }
 		if startDate == self && !returnNextIfAtBoundary { return self }
 		return startDate.drp_adding(1, component: component, calendar: calendar)
 	}
 	
-	public func drp_settingHour(to value: Int, calendar: Calendar = Calendar.current) -> Date? {
+	func drp_settingHour(to value: Int, calendar: Calendar = Calendar.current) -> Date? {
 		return calendar.date(bySettingHour: value, minute: 0, second: 0, of: self)
 	}
 	
-	public func drp_beginningOfShiftedDay(by shiftedHour: Int, calendar: Calendar = Calendar.current) -> Date? {
+	func drp_beginningOfShiftedDay(by shiftedHour: Int, calendar: Calendar = Calendar.current) -> Date? {
 		// We could also use calendar.nextDate(after:...) here, but that is significantly slower.
 		let modifiedDate = self.drp_settingHour(to: shiftedHour, calendar: calendar)
 		if let modifiedDate = modifiedDate, modifiedDate <= self {
@@ -100,7 +100,7 @@ public extension Date {
 		}
 	}
 	
-	public func drp_beginning(of component: Calendar.Component, hourShift: Int, calendar: Calendar = Calendar.current) -> Date? {
+	func drp_beginning(of component: Calendar.Component, hourShift: Int, calendar: Calendar = Calendar.current) -> Date? {
 		if !component.isAffectedByHourShift {
 			return self.drp_beginning(of: component, calendar: calendar)
 		}
@@ -111,7 +111,7 @@ public extension Date {
 			.drp_settingHour(to: hourShift, calendar: calendar)
 	}
 	
-	public func drp_end(of component: Calendar.Component, hourShift: Int, calendar: Calendar = Calendar.current) -> Date? {
+	func drp_end(of component: Calendar.Component, hourShift: Int, calendar: Calendar = Calendar.current) -> Date? {
 		if !component.isAffectedByHourShift {
 			return self.drp_end(of: component, calendar: calendar)
 		}
@@ -121,7 +121,7 @@ public extension Date {
 			.drp_adding(1, component: component, calendar: calendar)
 	}
 	
-	public func drp_components(_ component: Calendar.Component, since startDate: Date, calendar: Calendar = Calendar.current) -> Int? {
+	func drp_components(_ component: Calendar.Component, since startDate: Date, calendar: Calendar = Calendar.current) -> Int? {
 		guard let fromDate = startDate.drp_beginning(of: component, calendar: calendar),
 			let toDate = self.drp_beginning(of: component, calendar: calendar) else { return nil }
 		return calendar.dateComponents(Set(arrayLiteral: component), from: fromDate, to: toDate)
@@ -131,24 +131,24 @@ public extension Date {
 
 // Required for backwards compatibility with Objective-C.
 public extension NSDate {
-	public func drp_addCalendarUnits(_ count: Int, unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> NSDate? {
+	func drp_addCalendarUnits(_ count: Int, unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> NSDate? {
 		return (self as Date).drp_addCalendarUnits(count, unit: unit, calendar: calendar) as NSDate?
 	}
 	
-	public func drp_beginning(ofCalendarUnit unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> NSDate? {
+	func drp_beginning(ofCalendarUnit unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> NSDate? {
 		return (self as Date).drp_beginning(ofCalendarUnit: unit, calendar: calendar) as NSDate?
 	}
 	
-	public func drp_end(ofCalendarUnit unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> NSDate? {
+	func drp_end(ofCalendarUnit unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> NSDate? {
 		return (self as Date).drp_end(ofCalendarUnit: unit, calendar: calendar) as NSDate?
 	}
 	
-	public func drp_calendarUnits(since: NSDate, unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> Int? {
+	func drp_calendarUnits(since: NSDate, unit: NSCalendar.Unit, calendar: Calendar = Calendar.current) -> Int? {
 		return (self as Date).drp_calendarUnits(since: since as Date, unit: unit, calendar: calendar)
 	}
 	
 	// Returns the number of calendar days between the argument and the receiver.
-	public func drp_daysSince(_ since: NSDate, calendar: Calendar = Calendar.current) -> Int? {
+	func drp_daysSince(_ since: NSDate, calendar: Calendar = Calendar.current) -> Int? {
 		return (self as Date).drp_daysSince(since as Date, calendar: calendar)
 	}
 }
