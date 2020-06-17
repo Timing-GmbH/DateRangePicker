50f22adf2c11cb7048868b455acf24204a6525ce
Christian Tietze <christian.tietze@gmail.com>
2020-06-17 12:13:41 +0200

    add localization script

---
+++ localize.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+XLIFF_PATH=./tmp/xcode-xliff-export
+
+function export_xliff {
+  xcodebuild -exportLocalizations \
+      -project DateRangePicker.xcodeproj \
+      -localizationPath $XLIFF_PATH \
+      -exportLanguage en \
+      -exportLanguage de \
+      -exportLanguage zh-Hans \
+      -exportLanguage ru
+}
+
+function find_strings {
+  # Exclude framework and Xliff output directories
+  find . \
+    -type d \( -path ./tmp -o -path ./Frameworks -o -path ./Pods \) -prune \
+    -o -name \*.strings -print
+}
+
+function import_result {
+  if [ $# -eq 0 ]
+  then
+    print_help
+    exit 1
+  fi
+
+  rsync -av --prune-empty-dirs --include '*/' --include '*.strings' --exclude '*' "$1/" "."
+}
+
+function print_help {
+  echo -e "Usage:\t$0 [command [options ...]]"
+  echo ""
+  echo "Commands:"
+  echo -e "  strings\t\tList all .strings files in the project."
+  echo -e "  xliff\t\t\tExport Xliff for all localizations to ${XLIFF_PATH}"
+  echo -e "  import source_dir\tImports all .strings from source_dir, preserving directory structures"
+}
+
+# No command provided
+if [ $# -eq 0 ]
+then
+  print_help
+  exit 1
+fi
+
+# Execute each command
+case "$1"
+in
+"strings")
+  find_strings
+  ;;
+"xliff")
+  export_xliff
+  ;;
+"import")
+  shift
+  import_result $@
+  ;;
+
+"-h")
+  ;& # fallthrough
+"--help")
+  ;& # fallthrough
+"help")
+  ;& # fallthrough
+*)
+  print_help
+  ;;
+esac
