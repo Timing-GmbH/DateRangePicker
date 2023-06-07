2e4ea837e5cf901b93bedf5cf13a207da611a71a
Christian Tietze <me@christiantietze.de>
2023-06-07 23:50:31 +0200

    move Xcode-managed files into SPM folders

--- Sources/DateRangePicker/DoubleClickDateRangePicker.swift
+++ Sources/DateRangePicker/DoubleClickDateRangePicker.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2021 Daniel Alm. All rights reserved.
 //
 
-import Foundation
+import AppKit
 
 open class DoubleClickDateRangePicker: NSDatePicker {
 	open var doubleAction: Selector?
